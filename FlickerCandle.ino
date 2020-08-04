80abdb6e7741f2464928962a79391436f3ffa63c
Kyle Everly <everly@mmsi.com>
2020-08-04 10:43:37 -0700

    Added handler with starter functions, not working

--- FlickerCandle.ino
+++ FlickerCandle.ino
@@ -13,7 +13,7 @@ int t = 0;
 float norm(float mean, float sigma) {
   // takes mean and sigma (standard div) and returns a random value with a normal distribution probablity
   //return value=exp(-0.5*pow((x-mean)/sigma,2.));
-  return sqrt(-2*log(random(0,1))) * cos(2*PI*random(0,1)) * sigma + mean;
+  return sqrt(-2*log(random(0,1000)/1000.0)) * cos(2*PI*random(0,1000)/1000.0) * sigma + mean;
 }
 
 // CLASSES
@@ -21,19 +21,19 @@ class LED {
   private:
     int pin = -1;
     int FB = 255;  // full brightness / top level
-    float lvl = 200;  // the brightness level of the led
+    float lvl = 255;  // the brightness level of the led
     
     float pulse_rate = 1;
     float flicker_depth = 0;
     float urate = 0;  // up rate for current state
     float drate = 0;  // down rate for current state
-    float wave_depth;
+    int wave_depth;
     float wave_rate = 0;
-    float wave_duration = 0;  // in millis - with this actually be used?
+    // float wave_duration = 0;  // in millis - with this actually be used?
     
     unsigned short pulse_state = 0;
-    unsigned short flicker_state = 0;
-    unsigned short wave_state = 0;
+    unsigned short flicker_state = 2;
+    unsigned short wave_state = 2;
     unsigned short state = 1;
     /*
     * States:
@@ -42,6 +42,20 @@ class LED {
     * 2 - complete, unacknowledged
     * 3 - complete, acknowledged
     */
+
+    // FLICKER CONSTANTS, SPF is seconds per frame (technically milliseconds per frame)
+    // depth is the bottom of the dip (as opposed to the amount down from the top)
+    float const flicker_rate_down_mean = UPDATE_INT * 255 / 300;  // SPF * depth / time, in lvl per 5 ms
+    float const flicker_rate_down_sigma =  UPDATE_INT * 0.1;      // in lvl per 5 ms
+    float const flicker_rate_up_mean = UPDATE_INT * 255 / 700;    // SPF * depth / time, in lvl per 5 ms
+    float const flicker_rate_up_sigma = UPDATE_INT * 0.1;         // in lvl per 5 ms
+    int const flicker_depth_mean = 20;                            // in lvl
+    int const flicker_depth_sigma = 10;                           // in lvl
+    
+    float const wave_rate_mean = UPDATE_INT * 255 / 1320;         // SPF * depth / time, in lvl per 5 ms
+    float const wave_rate_sigma = UPDATE_INT * 10;                // SPT * depth / time, in lvl per 5 ms
+    int const wave_depth_mean = 100;                              // in lvl
+    int const wave_depth_sigma = 30;                             // in lvl
         
   public:
     LED(int led_pin);
@@ -51,8 +65,11 @@ class LED {
     void wave();
     void start_pulse(float rate);
     void start_flicker(float rateup, float ratedown, float depth);
-    void start_wave(float wave_rate, int duration);
-    bool is_event();  // returns if there was an event
+    void start_wave(float rate, int depth);
+    bool pulse_done();
+    bool flicker_done();
+    bool wave_done();
+    void handle_LED_events();
 };
 
 LED::LED(int led_pin) {
@@ -70,12 +87,19 @@ void LED::start_flicker(float rateup, float ratedown, float depth) {
   urate = rateup;
   drate = ratedown;
   flicker_depth = depth;
+  Serial.print("started flicker, rateup: ");
+  Serial.print(urate);
+  Serial.print(", ratedown: ");
+  Serial.print(drate);
+  Serial.print(", depth: ");
+  Serial.println(flicker_depth);
 }
 
-void LED::start_wave(float rate, int duration) {
+void LED::start_wave(float rate, int depth) {
   wave_state = 1;
   wave_rate = -1*abs(rate);
-  wave_duration = duration;
+  wave_depth = depth;
+  //wave_duration = duration;
 }
 
 void LED::pulse() {
@@ -129,19 +153,48 @@ void LED::update_led() {
   analogWrite(pin, lvl);
 }
 
-bool LED::is_event() {
-  if(pulse_state == 2 || flicker_state == 2 || wave_state == 2) {
+bool LED::pulse_done() {
+  if(pulse_state == 2) {
+    pulse_state = 3;
     return true;
   }
   return false;
 }
 
-// not sure the best way to handle events
+bool LED::flicker_done() {
+  if(flicker_state == 2) {
+    flicker_state = 3;
+    return true;
+  }
+  return false;
+}
+
+bool LED::wave_done() {
+  if(wave_state == 2) {
+    wave_state = 3;
+    return true;
+  }
+  return false;
+}
+
+void LED::handle_LED_events() {
+  /*if(this->pulse_done()) {
+    this->start_pulse(
+  } */
+  if(this->flicker_done()) {
+    this->start_flicker(norm(flicker_rate_up_mean, flicker_rate_up_sigma),
+                        norm(flicker_rate_down_mean, flicker_rate_down_sigma),
+                        norm(flicker_depth_mean, flicker_depth_sigma));
+  }
+  if(this->wave_done()) {
+    this->start_wave(norm(wave_rate_mean, wave_rate_sigma),
+                     norm(wave_depth_mean, wave_depth_sigma));
+  }
+}
 
 LED led1(3);
 LED led2(5);
 LED led3(6);
-
 
 
 // SET UP
@@ -152,6 +205,10 @@ void setup() {
 
 // MAIN
 void loop() {
+  led1.handle_LED_events();
+  led2.handle_LED_events();
+  led3.handle_LED_events();
+  
   led1.update_led();
   led2.update_led();
   led3.update_led();
